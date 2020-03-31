e16751e8477d6f19c9655efc1b1ef0ba1dfa88ba
Kevin Wittek <kevin@wittek.dev>
2020-03-31 14:01:08 +0200

    Use default value of -1 if retry limit hit in HistoricDataExtractor

--- src/test/java/de/internetsicherheit/brl/bloxberg/cache/HistoricDataExtractorTest.java
+++ src/test/java/de/internetsicherheit/brl/bloxberg/cache/HistoricDataExtractorTest.java
@@ -12,18 +12,32 @@ import static org.mockito.Mockito.when;
 
 
 public class HistoricDataExtractorTest {
-    /**
-     * tests the retry mechanism of the historicDataExtractor using a stub. however the test doesnt make much sense to me
-     */
-    @DisplayName("testing retry - not really.")
+
+
+    @DisplayName("retry works")
     @Test
     void canExtractBlockWithRetry() throws IOException {
         BloxbergClient mockClient = mock(BloxbergClient.class);
         HistoricDataExtractor extractor = new HistoricDataExtractor(mockClient, null, 0);
         int blockNumber = 1000;
+
         when(mockClient.getNumberOfTransactionsInBlock(BigInteger.valueOf(blockNumber)))
                 .thenThrow(new IOException())
                 .thenReturn(42);
+
         assertThat(extractor.getNumberOfTransactionsInBlockWithRetry(blockNumber)).isEqualTo(42);
+    }
+
+    @DisplayName("default value of -1 in case of retries exceeded")
+    @Test
+    void defaultValueIfRetryFails() throws IOException {
+        BloxbergClient mockClient = mock(BloxbergClient.class);
+        HistoricDataExtractor extractor = new HistoricDataExtractor(mockClient, null, 0);
+        int blockNumber = 1000;
+
+        when(mockClient.getNumberOfTransactionsInBlock(BigInteger.valueOf(blockNumber)))
+                .thenThrow(new IOException());
+
+        assertThat(extractor.getNumberOfTransactionsInBlockWithRetry(blockNumber)).isEqualTo(-1);
     }
 }
