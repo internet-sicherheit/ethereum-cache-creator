9c3e3699ecc5dff3260ee43e7cfda47fcdd9c807
Kevin Wittek <kevin@wittek.dev>
2020-03-31 14:01:08 +0200

    Use default value of -1 if retry limit hit in HistoricDataExtractor

--- src/main/java/de/internetsicherheit/brl/bloxberg/cache/HistoricDataExtractor.java
+++ src/main/java/de/internetsicherheit/brl/bloxberg/cache/HistoricDataExtractor.java
@@ -1,5 +1,6 @@
 package de.internetsicherheit.brl.bloxberg.cache;
 
+import org.rnorth.ducttape.RetryCountExceededException;
 import org.rnorth.ducttape.unreliables.Unreliables;
 
 import java.io.IOException;
@@ -23,7 +24,8 @@ public class HistoricDataExtractor {
     }
 
     /**
-     *  Extract Data from the Client ranging from the most recent block to a past block specified by a range
+     * Extract Data from the Client ranging from the most recent block to a past block specified by a range
+     *
      * @throws IOException exception when connection to blockchain cannot be established
      */
     public void extractData() throws IOException {
@@ -35,6 +37,7 @@ public class HistoricDataExtractor {
 
     /**
      * Extract all the Number of Transactions per block for the entire history of the blockchain. this might take a while.
+     *
      * @throws IOException exception when connection to blockchain cannot be established
      */
     public void extractAllData() throws IOException {
@@ -44,22 +47,29 @@ public class HistoricDataExtractor {
     }
 
     /**
-     *  extract the amount of transactions from a single block. if the program can't establish connection to the
-     *  blockchain it retries up to 10 times.
+     * Extract the amount of transactions from a single block.
+     *
+     * If the program can't establish connection to the
+     * network it retries it up to 10 times.
+     *
      * @param blockNumber the number of the block that the method extracts data from
      * @return amount of transactions in a single block
      */
     public int getNumberOfTransactionsInBlockWithRetry(int blockNumber) {
-        int transactions =
-                Unreliables.retryUntilSuccess(10, () ->
-                        client.getNumberOfTransactionsInBlock(BigInteger.valueOf(blockNumber))
-                );        return transactions;
+        try {
+            return Unreliables.retryUntilSuccess(10, () ->
+                    client.getNumberOfTransactionsInBlock(BigInteger.valueOf(blockNumber))
+            );
+        } catch (RetryCountExceededException e) {
+            return -1;
+        }
     }
 
 
     /**
      * this method merely tells the writer to write out
      * a combination of blocknumber and amount of transactions into a file specified in the writer
+     *
      * @param comb the combination of blocknumber and transactioncount as a custom datatype.
      */
     private void writeBlock(BlockWithTransactionCombination comb) {
