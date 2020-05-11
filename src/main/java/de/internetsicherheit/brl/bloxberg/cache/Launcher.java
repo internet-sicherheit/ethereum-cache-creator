03ecaee505876e225dff2d5d33a6e8f9551953c1
Kevin Wittek <kevin@wittek.dev>
2020-05-11 16:51:34 +0200

    Add building of fatJAR using maven-shade-plugin

---
+++ src/main/java/de/internetsicherheit/brl/bloxberg/cache/Launcher.java
@@ -0,0 +1,11 @@
+package de.internetsicherheit.brl.bloxberg.cache;
+
+import de.internetsicherheit.brl.bloxberg.cache.gui.MainGUI;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        MainGUI.main(args);
+    }
+
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module BloxbergCacheCreator {
-    requires duct.tape;
-    requires core;
-    requires com.fasterxml.jackson.databind;
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires jdk.xml.dom;
-}
