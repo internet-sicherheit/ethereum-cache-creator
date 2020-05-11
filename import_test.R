bbb9a6065077d9f01dbce10dd9012446f9b45f2a
beeshot <46483732+beeshot@users.noreply.github.com>
2020-05-11 19:18:26 +0200

    added a few things to R Script, not so advanced at this point (#19)
    * added a few things to R Script, not so advanced at this point
    
    * Fix extract.json
    
    * added ] and added it in script, plotting needs to be adapted, will check what @kview wrote in slack
    
    * Merge remote-tracking branch 'origin/tina_r' into tina_r
    
    Co-authored-by: Kevin Wittek <kiview@users.noreply.github.com>

--- import_test.R
+++ import_test.R
@@ -14,7 +14,7 @@ library("jsonlite")
 
 #import json file from local machine
 
-json_file <- "trans_mod.json"
+json_file <- "extract.json"
 
 json_data <- jsonlite::fromJSON( json_file )
 summary(json_data)
@@ -24,7 +24,46 @@ transaction <- data.frame(
   to=c (json_data$toAddress)
 )
 
-
-network <- graph_from_data_frame(d=transaction, directed=F)
+#undirected
+network <- graph_from_data_frame(d = transaction, directed=F)
 deg <- degree(network, mode="all")
-plot(network, vertex.size=deg*1, vertex.color=rgb(0.1,0.7,0.8,0.5) )
+plot(network, vertex.size=deg*0.1, vertex.color=rgb(0.1,0.7,0.8,0.5), vertex.label.cex = 0.5  )
+
+#Directed
+network <- graph_from_data_frame(d=transaction, directed=T)
+deg <- degree(network, mode="all")
+plot(network, vertex.size=deg*0.1, vertex.color=rgb(0.1,0.7,0.8,0.5), vertex.label.cex = 0.5  )
+
+#number of verticies in network
+gorder(network)
+
+#edges in network
+gsize(network)
+
+#self-loops, how would they look in data set?
+is.loop(network)
+
+# multiple edges of vertex per transaction?
+cm <- count.multiple(network, eids=E(network))
+boxplot (cm)
+plot (cm)
+
+
+# no. of edges per vertex including loops
+degree <- degree(network, v = V(network), mode = c("all", "out", "in", "total"),
+                 loops = TRUE, normalized = FALSE)
+
+#no of edges per vertex excluding loops
+degree_wl <- degree(network, v = V(network), mode = c("all", "out", "in", "total"),
+                 loops = FALSE, normalized = FALSE)
+
+#viz incl. loops
+plot (degree)
+boxplot (degree)
+
+#viz excluding loops
+plot (degree_wl)
+boxplot (degree_wl)
+
+
+
