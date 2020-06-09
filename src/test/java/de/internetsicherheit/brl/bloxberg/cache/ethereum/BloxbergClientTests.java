5e0d8cd6c0796ffba51858a53c72cf599bad74c8
lsl88 <42957594+lslada@users.noreply.github.com>
2020-06-09 14:55:21 +0200

    Fix missing JUnit import (#50)
    Co-authored-by: Laura Soledad Lazzati <lazzati@internet-sicherheit.de>

--- src/test/java/de/internetsicherheit/brl/bloxberg/cache/ethereum/BloxbergClientTests.java
+++ src/test/java/de/internetsicherheit/brl/bloxberg/cache/ethereum/BloxbergClientTests.java
@@ -1,6 +1,7 @@
 package de.internetsicherheit.brl.bloxberg.cache.ethereum;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
